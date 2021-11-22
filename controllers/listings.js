fa3a2bcc83e27e4496180e5dcf79da01e7a4c004
mauwjh <matthew.auw@gmail.com>
2021-11-22 13:00:42 +0800

    debug db.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const pool = require('../db')
 
 router.get('/recent', async (req, res) => {
-  result = await pool.query("SELECT listings.creator_id, listings.id, listings.title, listings.description, listings.category, listings.sale_option, listings.looking_for, listings.img, listings.price, categories.id as categories_id, categories.name as categories_name, users.username as users_username, users.email as users_email FROM listings JOIN categories ON categories.id = listings.category JOIN users ON users.id = listings.creator_id ORDER BY listings.id DESC LIMIT 9")
-  res.json(result)
+  try{
+    result = await pool.query("SELECT listings.creator_id, listings.id, listings.title, listings.description, listings.category, listings.sale_option, listings.looking_for, listings.img, listings.price, categories.id as categories_id, categories.name as categories_name, users.username as users_username, users.email as users_email FROM listings JOIN categories ON categories.id = listings.category JOIN users ON users.id = listings.creator_id ORDER BY listings.id DESC LIMIT 9")
+    res.json(result)
+  } catch(error) {
+    console.log(error)
+  }
 })
 
 router.get('/categories/:id/:limit', async (req, res) => {
--- db.js
+++ db.js
@@ -2,16 +2,26 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   host: process.env.PG_HOST,
+//   port: process.env.PG_PORT,
+//   database: process.env.PG_DATABASE
+// }
+
 const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE
+  user: 'irnjkizbyrpuvh',
+  password: 'a8783f99d118086c606143e36009f26cf0e4c628b9f7c7092af99b9160c3324c',
+  host: 'ec2-52-20-143-167.compute-1.amazonaws.com',
+  port: 5432,
+  database: 'dcjnpvo20nbgch', 
+  ssl: {required: true, rejectUnauthorized: false}
 }
 
 const proConfig = {
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {required: true, rejectUnauthorized: false}
 }
 
 const pool = new Pool(
